d043ad971b44c32664cada7a0e9707578e674d06
rajini-536 <dumparajani0@gmail.com>
2026-04-23 16:13:47 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main () 
+{
+    int a;
+   printf("Enter the A value \n");
+   scanf("%d",&a);
+   printf("A is not a +ve value i.e is:%d",a<0);
+   return 0;
+}
